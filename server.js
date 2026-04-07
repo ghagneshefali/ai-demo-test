bdd2d21d62e1374c5d8c5c03a40a1ed7732b427e
ghagneshefali <ghagneshefali@gmail.com>
2026-04-08 00:03:33 +0530

    Initialize Express server with a greeting route

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Ready for AI Modding.');
+});
+
+app.listen(3000, () => console.log('Server running on 3000'));
