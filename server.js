07bfb779c50c54d354fe3c21b45efc602497c3f0
ghagneshefali <ghagneshefali@gmail.com>
2026-04-08 02:41:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Ready for AI Modding.');
+  res.send('Hello World! Ready for AI Modification.');
 });
 
 app.listen(3000, () => console.log('Server running on 3000'));
